97a803a3d6230d9a8b958e30ed428bad2bfc1a6b
Bjørn Bråthen <bjorbrat88@gmail.com>
2014-03-10 14:02:51 +0100

    basic panel structure in CalendarView.java

--- src/client/view/CalendarView.java
+++ src/client/view/CalendarView.java
@@ -3,7 +3,6 @@ package view;
 import java.awt.Color;
 import java.awt.Dimension;
 
-import javax.swing.BorderFactory;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 
@@ -15,16 +14,22 @@ public class CalendarView extends JPanel {
 	
 	private JPanel topPanelWrapper = new JPanel();
 	private JLabel whereLabel = new JLabel(AppConstants.CALENDAR_HEADER_TEXT);
-	
+	private JPanel weeklyCalendarWrapper = new JPanel();
 	public CalendarView() {
 		
-		RelativeLayout rl = new RelativeLayout(RelativeLayout.X_AXIS, 0);
+		RelativeLayout rl = new RelativeLayout(RelativeLayout.Y_AXIS, 0);
+		setLayout(rl);
 		rl.setAlignment(RelativeLayout.LEADING);
 		topPanelWrapper.setLayout(rl);
 		topPanelWrapper.setBackground(Color.blue);
-		topPanelWrapper.setBorder(BorderFactory.createEmptyBorder(0, 0, 0, 0));
-		topPanelWrapper.setPreferredSize(new Dimension(800,200));
+		topPanelWrapper.setPreferredSize(new Dimension(AppConstants.MAIN_FRAME_WIDTH-AppConstants.SIDEBAR_WIDTH, AppConstants.HEADER_PANEL_HEIGHT));
+		weeklyCalendarWrapper.setBackground(Color.green);
+		weeklyCalendarWrapper.setPreferredSize(new Dimension(AppConstants.MAIN_FRAME_WIDTH-AppConstants.SIDEBAR_WIDTH,
+																AppConstants.MAIN_FRAME_HEIGHT-AppConstants.HEADER_PANEL_HEIGHT));
+		setPreferredSize(new Dimension(AppConstants.MAIN_FRAME_WIDTH-AppConstants.SIDEBAR_WIDTH,
+				AppConstants.MAIN_FRAME_HEIGHT));
 		
 		add(topPanelWrapper);
+		add(weeklyCalendarWrapper);
 	}
 }
