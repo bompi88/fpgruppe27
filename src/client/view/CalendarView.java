e20185abad6f5d1e262665136b7a14fec86b2be3
Bjørn Bråthen <bjorbrat88@gmail.com>
2014-03-10 13:30:12 +0100

    updated sql server path.

--- src/client/view/CalendarView.java
+++ src/client/view/CalendarView.java
@@ -1,15 +1,30 @@
 package view;
 
+import java.awt.Color;
+import java.awt.Dimension;
+
+import javax.swing.BorderFactory;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 import resources.AppConstants;
+import utils.RelativeLayout;
 
 @SuppressWarnings("serial")
 public class CalendarView extends JPanel {
+	
+	private JPanel topPanelWrapper = new JPanel();
 	private JLabel whereLabel = new JLabel(AppConstants.CALENDAR_HEADER_TEXT);
 	
 	public CalendarView() {
-		add(whereLabel);
+		
+		RelativeLayout rl = new RelativeLayout(RelativeLayout.X_AXIS, 0);
+		rl.setAlignment(RelativeLayout.LEADING);
+		topPanelWrapper.setLayout(rl);
+		topPanelWrapper.setBackground(Color.blue);
+		topPanelWrapper.setBorder(BorderFactory.createEmptyBorder(0, 0, 0, 0));
+		topPanelWrapper.setPreferredSize(new Dimension(800,200));
+		
+		add(topPanelWrapper);
 	}
 }
