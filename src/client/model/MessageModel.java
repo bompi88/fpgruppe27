7ef83ca3f760ba0ad9d24234176405ea4d5c398a
einaree <trippele@hotmail.com>
2014-03-10 13:27:57 +0100

    liten justering

--- src/client/model/MessageModel.java
+++ src/client/model/MessageModel.java
@@ -8,41 +8,41 @@ import framework.Model;
 public class MessageModel extends Model {
 	
 	protected Date date;
-	public Date getDate() {
-		return date;
-	}
-
-	public void setDate(Date date) {
-		this.date = date;
-	}
-
-	public String getMessage() {
-		return message;
-	}
-
-	public void setMessage(String message) {
-		this.message = message;
-	}
-
-	public MeetingModel getMeeting() {
-		return meeting;
-	}
-
-	public void setMeeting(MeetingModel meeting) {
-		this.meeting = meeting;
-	}
-
-	public boolean isSeen() {
-		return isSeen;
-	}
-
-	public void setSeen(boolean isSeen) {
-		this.isSeen = isSeen;
-	}
-
 	protected String message;
 	protected MeetingModel meeting;
 	protected boolean isSeen;
+	
+	public Date getDate() {
+		return date;
+	}
+	
+	public void setDate(Date date) {
+		this.date = date;
+	}
+	
+	public String getMessage() {
+		return message;
+	}
+	
+	public void setMessage(String message) {
+		this.message = message;
+	}
+	
+	public MeetingModel getMeeting() {
+		return meeting;
+	}
+	
+	public void setMeeting(MeetingModel meeting) {
+		this.meeting = meeting;
+	}
+	
+	public boolean isSeen() {
+		return isSeen;
+	}
+	
+	public void setSeen(boolean isSeen) {
+		this.isSeen = isSeen;
+	}
 
 	@Override
 	public void create() throws ClassNotFoundException, SQLException {
