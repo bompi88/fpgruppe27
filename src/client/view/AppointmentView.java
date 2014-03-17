374228306af3e1f9978472931c3bf74fde5fcb11
Andreas Drivenes <andreas.drivenes@gmail.com>
2014-03-17 11:37:14 +0100

    Legg til avtale fungerer! Koden er fortsatt litt fucka.

--- src/client/view/AppointmentView.java
+++ src/client/view/AppointmentView.java
@@ -2,9 +2,18 @@ package view;
 
 import java.awt.Dimension;
 import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 import javax.swing.BorderFactory;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
+
+import database.ClientObjectFactory;
 
 import resources.AppConstants;
 import utils.RelativeLayout;
@@ -23,21 +32,58 @@ public class AppointmentView extends JPanel {
 	private TitlePanel titlePanel;
 	private JPanel mainWrapper;
 	private Meeting model;
+	private JButton saveButton, cancelButton;
 	
 	public AppointmentView(Controller ctrl) {
 		
 		this.ctrl = ctrl;
-		ctrl.setModel(new Meeting());
+		//ctrl.setModel(new Meeting());
 		app = new AppointmentPanel(ctrl);
 		part = new ParticipantPanel();
+		model = new Meeting();
 		titlePanel = new TitlePanel();
+		saveButton = new JButton("Lagre");
 		mainWrapper = new JPanel();
 		mainWrapper.setLayout(new RelativeLayout(RelativeLayout.X_AXIS, 0));
 		mainWrapper.add(app);
 		mainWrapper.add(part);
+		model.addPropertyChangeListener(app);
+
 				
 		addUIElements();
 		
+		saveButton.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				if(app.checkInput()) {
+					model = app.createMeeting(model);
+					model.setParticipants(part.getParticipantList());
+					System.out.println(model);
+					ClientObjectFactory.addMeeting(model);
+				}
+				else {
+					JOptionPane.showMessageDialog(new JFrame(),"Vennligst fyll inn alle feltene.", "Mangler informasjon", JOptionPane.ERROR_MESSAGE);
+				}
+				
+				
+			}
+			
+		});
+		
+		
+	}
+
+	public void setModel(Meeting m) {
+		model.setName(m.getName());
+		model.setDescription(m.getDescription());
+		model.setStartTime(m.getStartTime());
+		model.setEndTime(m.getEndTime());
+		model.setPlace(m.getPlace());
+		model.setParticipants(m.getParticipants());
+		model.setMeetid(m.getMeetid());
+		model.setResponsible(m.getResponsible());
+		model.setRoom(m.getRoom());
 		
 	}
 	
@@ -51,22 +97,17 @@ public class AppointmentView extends JPanel {
 		rl1.setAlignment(RelativeLayout.LEADING);
 		setLayout(rl1);
 		add(titlePanel);
-		//add(app);
-		//add(part);
 		add(mainWrapper);
+		add(saveButton);
+	
 		
 		
 	}
-	
-	private boolean checkInput() {
-		return true;
-	}
-	
 	class TitlePanel extends JPanel {
-		protected JTextField title;
+		private JLabel title;
 		public TitlePanel() {
 			setBorder(BorderFactory.createEmptyBorder(30, -2, 0, 0));
-			title = new JTextField("MØTENAVN");
+			title = new JLabel("Opprett avtale");
 			title.setFont(new Font("Arial", Font.PLAIN, 28));
 			add(title);
 			setBackground(AppConstants.HEADER_BG_COLOR);
